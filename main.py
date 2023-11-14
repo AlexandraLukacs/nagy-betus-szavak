e7c15180cadd5bcd00468d4b614e77164c699ff4
AlexandraLukacs <lszandika15@gmail.com>
2023-11-14 14:10:57 +0100

    nagy betus szavak

---
+++ main.py
@@ -0,0 +1,3 @@
+import szovegkezeles
+
+szovegkezeles.feladat()
---
+++ szovegkezeles.py
@@ -0,0 +1,9 @@
+def feladat():
+    '''Alakíts át egy mondatot úgy, hogy minden szó kezdőbetűjét alakítsd nagybetűssé.
+            String s = "ez egy mondat lenne, melynek minden szavat nagy "
+                "kezdobetusse kellene alakitani.";
+            Eredmény:
+                Ez Egy Mondat Lenne, Melynek Minden Szavat Nagy Kezdobetusse Kellene Alakitani.'''
+    print("Feladat")
+    s: str="Ez egy mondat lenne, melynek minden szavat nagy kezdobetusse kellene alakitani."
+    print(s.title())
